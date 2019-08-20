d739d5d5d9cf9df13ddc9bcdbb1cb07ce716b745
Enrique Crespo <jecrespom@gmail.com>
2019-08-20 11:16:37 +0200

    Serial update

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -2,12 +2,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Encendido");
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("Apagado");
   delay(1000);                       // wait for a second
 }
